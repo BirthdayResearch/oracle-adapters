1ec70b8c28af1a46cec824822f63373100ab1030
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-10-06 23:58:22 +0800

    setup mono-repo package json (#2)

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testRegex: '((\\.|/)(e2e|test|spec))\\.[jt]sx?$',
+  moduleNameMapper: {
+    '@defichain/salmon-(.*)': '<rootDir>/packages/salmon-$1/src'
+  },
+  verbose: true,
+  clearMocks: true,
+  testTimeout: 180000,
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '.*/__tests__/.*'
+  ]
+}
