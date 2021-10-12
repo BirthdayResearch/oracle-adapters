f7d039a07841d4163ee789fb384f41e7a48256f4
Ahmed Hilali <monstrobishi@gmail.com>
2021-10-12 19:08:43 +0800

    Fix setup

--- e2e/setup.ts
+++ e2e/setup.ts
@@ -6,11 +6,10 @@ export const oracleOwner = GenesisKeys[GenesisKeys.length - 1].operator
 export const rpcClient = new PlaygroundRpcClient(new PlaygroundApiClient({ url: 'http://localhost:3002' }))
 
 export async function setupOracle (symbols: string[]): Promise<string> {
-  const oracleId = await exports.rpcClient.oracle.appointOracle(exports.oracleOwner.address, [
-    symbols.map(x => ({ token: x, currency: 'USD' }))
-  ], {
-    weightage: 1.0
-  })
+  const oracleId = await exports.rpcClient.oracle.appointOracle(exports.oracleOwner.address,
+    symbols.map(x => ({ token: x, currency: 'USD' })), {
+      weightage: 1.0
+    })
 
   await waitForExpect(async () => {
     expect((await exports.rpcClient.wallet.getTransaction(oracleId)).confirmations).toBeGreaterThanOrEqual(3)
