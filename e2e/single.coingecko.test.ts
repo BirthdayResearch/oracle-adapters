121623a4d0c12febf4c67e361e5d4d7e213d7c71
Ahmed Hilali <monstrobishi@gmail.com>
2021-10-12 20:33:22 +0800

    Fix test

--- e2e/single.coingecko.test.ts
+++ e2e/single.coingecko.test.ts
@@ -17,7 +17,8 @@ describe('e2e single coingecko', () => {
     const client = new WhaleApiClient({ url: 'http://localhost:3001', network: 'regtest', version: 'v0' })
     const wallet = new SalmonWallet(oracleOwner.privKey, 'regtest', client)
     const salmon = new Salmon(oracleId, 'regtest', client, wallet)
-    await salmon.publish(await coingecko(['BTC', 'ETH', 'DOGE']))
+    const coingeckoPrices = await coingecko(['BTC', 'ETH', 'DOGE'])
+    await salmon.publish(coingeckoPrices)
 
     await waitForExpect(async () => {
       expect(
@@ -26,8 +27,46 @@ describe('e2e single coingecko', () => {
     }, 10000)
 
     const oracleData = await rpcClient.oracle.getOracleData(oracleId)
-    expect(oracleData.tokenPrices[0].amount).toStrictEqual(120)
-    expect(oracleData.tokenPrices[1].amount).toStrictEqual(330)
-    expect(oracleData.tokenPrices[2].amount).toStrictEqual(605)
+    expect(oracleData).toStrictEqual(
+      {
+        address: '0014e0dc4bafbea4d48f538ae526d8143a7159a510ee',
+        oracleid: oracleId,
+        priceFeeds: [
+          {
+            currency: 'USD',
+            token: 'BTC'
+          },
+          {
+            currency: 'USD',
+            token: 'DOGE'
+          },
+          {
+            currency: 'USD',
+            token: 'ETH'
+          }
+        ],
+        tokenPrices: [
+          {
+            amount: coingeckoPrices[0].amount.toNumber(),
+            currency: 'USD',
+            timestamp: expect.any(Number),
+            token: 'BTC'
+          },
+          {
+            amount: coingeckoPrices[2].amount.toNumber(),
+            currency: 'USD',
+            timestamp: expect.any(Number),
+            token: 'DOGE'
+          },
+          {
+            amount: coingeckoPrices[1].amount.toNumber(),
+            currency: 'USD',
+            timestamp: expect.any(Number),
+            token: 'ETH'
+          }
+        ],
+        weightage: 1
+      }
+    )
   })
 })
