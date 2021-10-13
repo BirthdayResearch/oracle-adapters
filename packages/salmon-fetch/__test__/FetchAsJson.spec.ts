b5c691497e1ad819b30497456047e5d8fec40019
Joel-David Wong <git@joeldavid.dev>
2021-10-14 03:43:31 +0800

    Added string check in FetchAsJson.spec.ts

--- packages/salmon-fetch/__test__/FetchAsJson.spec.ts
+++ packages/salmon-fetch/__test__/FetchAsJson.spec.ts
@@ -17,12 +17,14 @@ describe('single asset price fetch', () => {
             dfi: {
               usd: 0.208377
             },
-            doge: 123
+            doge: 123,
+            currency: 'USD'
           }
         })
       const price = await fetchAsJson('https://test.defichain.endpoint/')
       expect(price.data.dfi.usd).toStrictEqual(new BigNumber(0.208377))
       expect(price.data.doge).toStrictEqual(new BigNumber(123))
+      expect(price.data.currency).toStrictEqual('USD')
     })
 
     it('should timeout after 1 second', async () => {
