d36780f57a329a3318767e0f98850b6c841c30d3
Joel-David Wong <git@joeldavid.dev>
2021-10-14 17:22:41 +0800

    Added more type checks

--- packages/salmon-fetch/__test__/AssetPrice.spec.ts
+++ packages/salmon-fetch/__test__/AssetPrice.spec.ts
@@ -1,7 +1,7 @@
 import BigNumber from 'bignumber.js'
 import { newAssetPrice } from '@defichain/salmon-fetch'
 
-describe('PriceAsset interface', () => {
+describe('AssetPrice.amount', () => {
   it('should include valid price - string', () => {
     expect(newAssetPrice('APPL', '1', 'USD', 0)).toStrictEqual({
       amount: new BigNumber('1'),
@@ -29,14 +29,6 @@ describe('PriceAsset interface', () => {
     })
   })
 
-  it('should exclude invalid token - not string', () => {
-    const json = JSON.parse('{"test": 12}')
-
-    expect(() => {
-      newAssetPrice(json.test, '$1.00', 'USD', 0)
-    }).toThrowError(Error('token is not string'))
-  })
-
   it('should exclude invalid price - string', () => {
     expect(() => {
       newAssetPrice('APPL', '$1.00', 'USD', 0)
@@ -55,7 +47,67 @@ describe('PriceAsset interface', () => {
     }).toThrowError(Error('price is not string, number of BigNumber'))
   })
 
-  it('should exclude invalid currency - not string', () => {
+  it('should exclude invalid price - array', () => {
+    expect(() => {
+      newAssetPrice('APPL', [] as any, 'USD', 0)
+    }).toThrowError(Error('price is not string, number of BigNumber'))
+  })
+
+  it('should exclude invalid price - JSON', () => {
+    expect(() => {
+      newAssetPrice('APPL', JSON.parse('{}'), 'USD', 0)
+    }).toThrowError(Error('price is not string, number of BigNumber'))
+  })
+
+  it('should exclude invalid price - Date', () => {
+    expect(() => {
+      newAssetPrice('APPL', new Date() as any, 'USD', 0)
+    }).toThrowError(Error('price is not string, number of BigNumber'))
+  })
+
+  it('should exclude invalid price - null', () => {
+    expect(() => {
+      newAssetPrice('APPL', null as any, 'USD', 0)
+    }).toThrowError(Error('price is not string, number of BigNumber'))
+  })
+})
+
+describe('AssetPrice.token', () => {
+  it('should exclude invalid token - not string', () => {
+    const json = JSON.parse('{"test": 12}')
+
+    expect(() => {
+      newAssetPrice(json.test, '$1.00', 'USD', 0)
+    }).toThrowError(Error('token is not string'))
+  })
+
+  it('should exclude invalid token - array', () => {
+    expect(() => {
+      newAssetPrice([] as any, '1', 'USD', 0)
+    }).toThrowError(Error('token is not string'))
+  })
+
+  it('should exclude invalid token - null', () => {
+    expect(() => {
+      newAssetPrice(null as any, '1', 'USD', 0)
+    }).toThrowError(Error('token is not string'))
+  })
+
+  it('should exclude invalid token - number', () => {
+    expect(() => {
+      newAssetPrice(123 as any, '1', 'USD', 0)
+    }).toThrowError(Error('token is not string'))
+  })
+
+  it('should exclude invalid token - BigNumber', () => {
+    expect(() => {
+      newAssetPrice(new BigNumber(123) as any, '1', 'USD', 0)
+    }).toThrowError(Error('token is not string'))
+  })
+})
+
+describe('AssetPrice.currency', () => {
+  it('should exclude invalid currency - JSON', () => {
     const json = JSON.parse('{"test": 12}')
 
     expect(() => {
@@ -63,6 +115,32 @@ describe('PriceAsset interface', () => {
     }).toThrowError(Error('currency is not string'))
   })
 
+  it('should exclude invalid currency - array', () => {
+    expect(() => {
+      newAssetPrice('APPL', '1', [] as any, 0)
+    }).toThrowError(Error('currency is not string'))
+  })
+
+  it('should exclude invalid currency - null', () => {
+    expect(() => {
+      newAssetPrice('APPL', '1', null as any, 0)
+    }).toThrowError(Error('currency is not string'))
+  })
+
+  it('should exclude invalid currency - number', () => {
+    expect(() => {
+      newAssetPrice('APPL', '1', 123 as any, 0)
+    }).toThrowError(Error('currency is not string'))
+  })
+
+  it('should exclude invalid currency - BigNumber', () => {
+    expect(() => {
+      newAssetPrice('APPL', '1', new BigNumber(123) as any, 0)
+    }).toThrowError(Error('currency is not string'))
+  })
+})
+
+describe('AssetPrice.timestamp', () => {
   it('should include valid timestamp - number', () => {
     const timestamp = Date.now()
     expect(newAssetPrice('APPL', 1, 'USD', timestamp)).toStrictEqual({
@@ -92,6 +170,36 @@ describe('PriceAsset interface', () => {
   it('should exclude invalid timestamp - Infinity', () => {
     expect(() => {
       newAssetPrice('APPL', '1', 'USD', new BigNumber(Infinity))
+    }).toThrowError(Error('timestamp is not number or BigNumber'))
+  })
+
+  it('should exclude invalid timestamp - string', () => {
+    expect(() => {
+      newAssetPrice('APPL', '1', 'USD', 'string' as any)
+    }).toThrowError(Error('timestamp is not number or BigNumber'))
+  })
+
+  it('should exclude invalid timestamp - array', () => {
+    expect(() => {
+      newAssetPrice('APPL', '1', 'USD', [] as any)
+    }).toThrowError(Error('timestamp is not number or BigNumber'))
+  })
+
+  it('should exclude invalid timestamp - JSON', () => {
+    expect(() => {
+      newAssetPrice('APPL', '1', 'USD', JSON.parse('{}'))
+    }).toThrowError(Error('timestamp is not number or BigNumber'))
+  })
+
+  it('should exclude invalid timestamp - Date', () => {
+    expect(() => {
+      newAssetPrice('APPL', '1', 'USD', new Date() as any)
+    }).toThrowError(Error('timestamp is not number or BigNumber'))
+  })
+
+  it('should exclude invalid timestamp - null', () => {
+    expect(() => {
+      newAssetPrice('APPL', '1', 'USD', null as any)
     }).toThrowError(Error('timestamp is not number or BigNumber'))
   })
 })
--- packages/salmon-fetch/__test__/FetchAsJson.spec.ts
+++ packages/salmon-fetch/__test__/FetchAsJson.spec.ts
@@ -2,43 +2,41 @@ import { fetchAsJson } from '../src'
 import nock from 'nock'
 import { BigNumber } from '@defichain/jellyfish-json'
 
-describe('single asset price fetch', () => {
+describe('FetchAsJson', () => {
   afterEach(() => {
     jest.clearAllMocks()
     nock.cleanAll()
   })
 
-  describe('FetchAsJson', () => {
-    it('should convert all number to BigNumber', async () => {
-      nock('https://test.defichain.endpoint')
-        .get('/')
-        .reply(200, function (_) {
-          return {
-            dfi: {
-              usd: 0.208377
-            },
-            doge: 123,
-            currency: 'USD'
-          }
-        })
-      const price = await fetchAsJson('https://test.defichain.endpoint/')
-      expect(price.data.dfi.usd).toStrictEqual(new BigNumber(0.208377))
-      expect(price.data.doge).toStrictEqual(new BigNumber(123))
-      expect(price.data.currency).toStrictEqual('USD')
-    })
-
-    it('should timeout after 1 second', async () => {
-      nock('https://test.defichain.endpoint')
-        .get('/')
-        .delay(2000)
-        .reply(200, function (_) {
-          return 'Aborted'
-        })
-
-      const promise = fetchAsJson('https://test.defichain.endpoint/', {
-        timeout: 1000
+  it('should convert all number to BigNumber', async () => {
+    nock('https://test.defichain.endpoint')
+      .get('/')
+      .reply(200, function (_) {
+        return {
+          dfi: {
+            usd: 0.208377
+          },
+          doge: 123,
+          currency: 'USD'
+        }
       })
-      await expect(promise).rejects.toThrow('The user aborted a request.')
+    const price = await fetchAsJson('https://test.defichain.endpoint/')
+    expect(price.data.dfi.usd).toStrictEqual(new BigNumber(0.208377))
+    expect(price.data.doge).toStrictEqual(new BigNumber(123))
+    expect(price.data.currency).toStrictEqual('USD')
+  })
+
+  it('should timeout after 1 second', async () => {
+    nock('https://test.defichain.endpoint')
+      .get('/')
+      .delay(2000)
+      .reply(200, function (_) {
+        return 'Aborted'
+      })
+
+    const promise = fetchAsJson('https://test.defichain.endpoint/', {
+      timeout: 1000
     })
+    await expect(promise).rejects.toThrow('The user aborted a request.')
   })
 })
