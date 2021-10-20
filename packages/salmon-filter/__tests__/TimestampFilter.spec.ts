0381342cefbe45e09f19aa02515c22a491fcc53c
Joel-David Wong <git@joeldavid.dev>
2021-10-20 12:53:45 +0800

    Added customizable options for TimestampFilter

--- packages/salmon-filter/__tests__/TimestampFilter.spec.ts
+++ packages/salmon-filter/__tests__/TimestampFilter.spec.ts
@@ -6,88 +6,118 @@ import { TimestampFilter } from '../src/filters/TimestampFilter'
 const container = new WhaleMasternodeRegTestContainer()
 let filter: AbstractFilter
 
-beforeAll(async () => {
-  await container.start()
+describe('default TimestampFilter', () => {
+  beforeAll(async () => {
+    await container.start()
 
-  const network = 'regtest'
-  const client = container.getWhaleApiClient()
-  const oracleId = '0000000000000000000000000000000000000000000000000000000000000000'
-  filter = new TimestampFilter(network, client, oracleId)
-})
+    const network = 'regtest'
+    const client = container.getWhaleApiClient()
+    const oracleId = '0000000000000000000000000000000000000000000000000000000000000000'
+    filter = new TimestampFilter(network, client, oracleId)
+  })
 
-afterAll(async () => {
-  await container.stop()
-})
+  afterAll(async () => {
+    await container.stop()
+  })
 
-it('should error on NaN', async () => {
-  await expect(async () => {
-    await filter.call([
-      {
-        token: 'TSLA',
-        amount: new BigNumber(1),
-        currency: 'USD',
-        timestamp: new BigNumber(NaN)
-      }
-    ])
-  }).rejects.toThrowError('TimestampFilter.isInvalid ')
-})
+  it('should error on NaN', async () => {
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: new BigNumber(NaN)
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
 
-it('should error on Infinity', async () => {
-  await expect(async () => {
-    await filter.call([
-      {
-        token: 'TSLA',
-        amount: new BigNumber(1),
-        currency: 'USD',
-        timestamp: new BigNumber(Infinity)
-      }
-    ])
-  }).rejects.toThrowError('TimestampFilter.isInvalid ')
-})
+  it('should error on Infinity', async () => {
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: new BigNumber(Infinity)
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
 
-it('should error on -Infinity', async () => {
-  await expect(async () => {
-    await filter.call([
-      {
-        token: 'TSLA',
-        amount: new BigNumber(1),
-        currency: 'USD',
-        timestamp: new BigNumber(-Infinity)
-      }
-    ])
-  }).rejects.toThrowError('TimestampFilter.isInvalid ')
-})
+  it('should error on -Infinity', async () => {
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: new BigNumber(-Infinity)
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
 
-it('should error on -1', async () => {
-  await expect(async () => {
-    await filter.call([
-      {
-        token: 'TSLA',
-        amount: new BigNumber(1),
-        currency: 'USD',
-        timestamp: new BigNumber(-1)
-      }
-    ])
-  }).rejects.toThrowError('TimestampFilter.isInvalid ')
-})
+  it('should error on -1', async () => {
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: new BigNumber(-1)
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
 
-it('should error on Zero', async () => {
-  await expect(async () => {
-    await filter.call([
-      {
-        token: 'TSLA',
-        amount: new BigNumber(1),
-        currency: 'USD',
-        timestamp: new BigNumber('0.0')
-      }
-    ])
-  }).rejects.toThrowError('TimestampFilter.isInvalid ')
-})
+  it('should error on Zero', async () => {
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: new BigNumber('0.0')
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
 
-it('should error on older than 3 weeks', async () => {
-  const timestamp = new BigNumber(Date.now()).minus((1000 * 60 * 60 * 24 * 7 * 3) + 1000)
+  it('should error on older than 3 weeks', async () => {
+    const timestamp = new BigNumber(Date.now()).minus((1000 * 60 * 60 * 24 * 7 * 3) + 1000)
 
-  await expect(async () => {
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: timestamp
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
+
+  it('should error on more than 3 weeks into the future', async () => {
+    const timestamp = new BigNumber(Date.now()).plus((1000 * 60 * 60 * 24 * 7 * 3) + 1)
+
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: timestamp
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
+
+  it('should allow 5 mins old', async () => {
+    const timestamp = new BigNumber(Date.now()).minus(1000 * 60 * 5)
+
     await filter.call([
       {
         token: 'TSLA',
@@ -96,13 +126,11 @@ it('should error on older than 3 weeks', async () => {
         timestamp: timestamp
       }
     ])
-  }).rejects.toThrowError('TimestampFilter.isInvalid ')
-})
+  })
 
-it('should error on more than 3 weeks into the future', async () => {
-  const timestamp = new BigNumber(Date.now()).plus((1000 * 60 * 60 * 24 * 7 * 3) + 1)
+  it('should allow 1 week old', async () => {
+    const timestamp = new BigNumber(Date.now()).minus(1000 * 60 * 60 * 24 * 7)
 
-  await expect(async () => {
     await filter.call([
       {
         token: 'TSLA',
@@ -111,128 +139,348 @@ it('should error on more than 3 weeks into the future', async () => {
         timestamp: timestamp
       }
     ])
-  }).rejects.toThrowError('TimestampFilter.isInvalid ')
-})
+  })
 
-it('should allow 5 mins old', async () => {
-  const timestamp = new BigNumber(Date.now()).minus(1000 * 60 * 5)
+  it('should allow 2 weeks old', async () => {
+    const timestamp = new BigNumber(Date.now()).minus(1000 * 60 * 60 * 24 * 7 * 2)
 
-  await filter.call([
-    {
-      token: 'TSLA',
-      amount: new BigNumber(1),
-      currency: 'USD',
-      timestamp: timestamp
-    }
-  ])
-})
-
-it('should allow 1 week old', async () => {
-  const timestamp = new BigNumber(Date.now()).minus(1000 * 60 * 60 * 24 * 7)
-
-  await filter.call([
-    {
-      token: 'TSLA',
-      amount: new BigNumber(1),
-      currency: 'USD',
-      timestamp: timestamp
-    }
-  ])
-})
-
-it('should allow 2 weeks old', async () => {
-  const timestamp = new BigNumber(Date.now()).minus(1000 * 60 * 60 * 24 * 7 * 2)
-
-  await filter.call([
-    {
-      token: 'TSLA',
-      amount: new BigNumber(1),
-      currency: 'USD',
-      timestamp: timestamp
-    }
-  ])
-})
-
-it('should allow 3 weeks old', async () => {
-  const timestamp = new BigNumber(Date.now()).minus(1000 * 60 * 60 * 24 * 7 * 3)
-
-  await filter.call([
-    {
-      token: 'TSLA',
-      amount: new BigNumber(1),
-      currency: 'USD',
-      timestamp: timestamp
-    }
-  ])
-})
-
-it('should allow 5 mins into the future', async () => {
-  const timestamp = new BigNumber(Date.now()).plus(1000 * 60 * 5)
-
-  await filter.call([
-    {
-      token: 'TSLA',
-      amount: new BigNumber(1),
-      currency: 'USD',
-      timestamp: timestamp
-    }
-  ])
-})
-
-it('should allow 1 week into the future', async () => {
-  const timestamp = new BigNumber(Date.now()).plus(1000 * 60 * 60 * 24 * 7)
-
-  await filter.call([
-    {
-      token: 'TSLA',
-      amount: new BigNumber(1),
-      currency: 'USD',
-      timestamp: timestamp
-    }
-  ])
-})
-
-it('should allow 2 weeks into the future', async () => {
-  const timestamp = new BigNumber(Date.now()).plus(1000 * 60 * 60 * 24 * 7 * 2)
-
-  await filter.call([
-    {
-      token: 'TSLA',
-      amount: new BigNumber(1),
-      currency: 'USD',
-      timestamp: timestamp
-    }
-  ])
-})
-
-it('should allow 3 weeks into the future', async () => {
-  const timestamp = new BigNumber(Date.now()).plus(1000 * 60 * 60 * 24 * 7 * 3)
-
-  await filter.call([
-    {
-      token: 'TSLA',
-      amount: new BigNumber(1),
-      currency: 'USD',
-      timestamp: timestamp
-    }
-  ])
-})
-
-it('should error and fail if one is invalid', async () => {
-  await expect(async () => {
     await filter.call([
       {
         token: 'TSLA',
         amount: new BigNumber(1),
         currency: 'USD',
-        timestamp: new BigNumber(Date.now())
-      },
+        timestamp: timestamp
+      }
+    ])
+  })
+
+  it('should allow 3 weeks old', async () => {
+    const timestamp = new BigNumber(Date.now()).minus(1000 * 60 * 60 * 24 * 7 * 3)
+
+    await filter.call([
       {
         token: 'TSLA',
         amount: new BigNumber(1),
         currency: 'USD',
-        timestamp: new BigNumber(-1)
+        timestamp: timestamp
       }
     ])
-  }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
+
+  it('should allow 5 mins into the future', async () => {
+    const timestamp = new BigNumber(Date.now()).plus(1000 * 60 * 5)
+
+    await filter.call([
+      {
+        token: 'TSLA',
+        amount: new BigNumber(1),
+        currency: 'USD',
+        timestamp: timestamp
+      }
+    ])
+  })
+
+  it('should allow 1 week into the future', async () => {
+    const timestamp = new BigNumber(Date.now()).plus(1000 * 60 * 60 * 24 * 7)
+
+    await filter.call([
+      {
+        token: 'TSLA',
+        amount: new BigNumber(1),
+        currency: 'USD',
+        timestamp: timestamp
+      }
+    ])
+  })
+
+  it('should allow 2 weeks into the future', async () => {
+    const timestamp = new BigNumber(Date.now()).plus(1000 * 60 * 60 * 24 * 7 * 2)
+
+    await filter.call([
+      {
+        token: 'TSLA',
+        amount: new BigNumber(1),
+        currency: 'USD',
+        timestamp: timestamp
+      }
+    ])
+  })
+
+  it('should allow 3 weeks into the future', async () => {
+    const timestamp = new BigNumber(Date.now()).plus(1000 * 60 * 60 * 24 * 7 * 3)
+
+    await filter.call([
+      {
+        token: 'TSLA',
+        amount: new BigNumber(1),
+        currency: 'USD',
+        timestamp: timestamp
+      }
+    ])
+  })
+
+  it('should error and fail if one is invalid', async () => {
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: new BigNumber(Date.now())
+        },
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: new BigNumber(-1)
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
+})
+
+describe('TimestampFilter with custom options (maxAge: 2 week, minAge: 1 week)', () => {
+  beforeAll(async () => {
+    await container.start()
+
+    const network = 'regtest'
+    const client = container.getWhaleApiClient()
+    const oracleId = '0000000000000000000000000000000000000000000000000000000000000000'
+
+    const maxAge = new BigNumber(1000 * 60 * 60 * 24 * 7 * 2)
+    const minAge = new BigNumber(1000 * 60 * 60 * 24 * 7).negated()
+
+    filter = new TimestampFilter(network, client, oracleId, {
+      maxAge: maxAge,
+      minAge: minAge
+    })
+  })
+
+  afterAll(async () => {
+    await container.stop()
+  })
+
+  it('should error on NaN', async () => {
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: new BigNumber(NaN)
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
+
+  it('should error on Infinity', async () => {
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: new BigNumber(Infinity)
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
+
+  it('should error on -Infinity', async () => {
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: new BigNumber(-Infinity)
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
+
+  it('should error on -1', async () => {
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: new BigNumber(-1)
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
+
+  it('should error on Zero', async () => {
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: new BigNumber('0.0')
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
+
+  it('should error on older than 2 weeks', async () => {
+    const timestamp = new BigNumber(Date.now()).minus((1000 * 60 * 60 * 24 * 7 * 2) + 1)
+
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: timestamp
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
+
+  it('should error on older than 3 weeks', async () => {
+    const timestamp = new BigNumber(Date.now()).minus((1000 * 60 * 60 * 24 * 7 * 3))
+
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: timestamp
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
+
+  it('should error on more than 1 week into the future', async () => {
+    const timestamp = new BigNumber(Date.now()).plus((1000 * 60 * 60 * 24 * 7) + 1)
+
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: timestamp
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
+
+  it('should error on 2 weeks into the future', async () => {
+    const timestamp = new BigNumber(Date.now()).plus((1000 * 60 * 60 * 24 * 7 * 2))
+
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: timestamp
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
+
+  it('should error on 3 weeks into the future', async () => {
+    const timestamp = new BigNumber(Date.now()).plus((1000 * 60 * 60 * 24 * 7 * 3))
+
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: timestamp
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
+
+  it('should allow 5 mins old', async () => {
+    const timestamp = new BigNumber(Date.now()).minus(1000 * 60 * 5)
+
+    await filter.call([
+      {
+        token: 'TSLA',
+        amount: new BigNumber(1),
+        currency: 'USD',
+        timestamp: timestamp
+      }
+    ])
+  })
+
+  it('should allow 1 week old', async () => {
+    const timestamp = new BigNumber(Date.now()).minus(1000 * 60 * 60 * 24 * 7)
+
+    await filter.call([
+      {
+        token: 'TSLA',
+        amount: new BigNumber(1),
+        currency: 'USD',
+        timestamp: timestamp
+      }
+    ])
+  })
+
+  it('should allow 2 weeks old', async () => {
+    const timestamp = new BigNumber(Date.now()).minus(1000 * 60 * 60 * 24 * 7 * 2)
+
+    await filter.call([
+      {
+        token: 'TSLA',
+        amount: new BigNumber(1),
+        currency: 'USD',
+        timestamp: timestamp
+      }
+    ])
+  })
+
+  it('should allow 5 mins into the future', async () => {
+    const timestamp = new BigNumber(Date.now()).plus(1000 * 60 * 5)
+
+    await filter.call([
+      {
+        token: 'TSLA',
+        amount: new BigNumber(1),
+        currency: 'USD',
+        timestamp: timestamp
+      }
+    ])
+  })
+
+  it('should allow 1 week into the future', async () => {
+    const timestamp = new BigNumber(Date.now()).plus(1000 * 60 * 60 * 24 * 7)
+
+    await filter.call([
+      {
+        token: 'TSLA',
+        amount: new BigNumber(1),
+        currency: 'USD',
+        timestamp: timestamp
+      }
+    ])
+  })
+
+  it('should error and fail if one is invalid', async () => {
+    await expect(async () => {
+      await filter.call([
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: new BigNumber(Date.now())
+        },
+        {
+          token: 'TSLA',
+          amount: new BigNumber(1),
+          currency: 'USD',
+          timestamp: new BigNumber(-1)
+        }
+      ])
+    }).rejects.toThrowError('TimestampFilter.isInvalid ')
+  })
 })
--- packages/salmon-filter/src/filters/TimestampFilter.ts
+++ packages/salmon-filter/src/filters/TimestampFilter.ts
@@ -1,15 +1,36 @@
 import { AbstractFilter } from '../AbstractFilter'
 import { AssetPrice } from '@defichain/salmon-fetch'
 import BigNumber from 'bignumber.js'
+import { NetworkName } from '@defichain/jellyfish-network'
+import { WhaleApiClient } from '@defichain/whale-api-client'
+
+export interface TimestampFilterOptions {
+  maxAge?: BigNumber
+  minAge?: BigNumber
+}
 
 /**
  * TimestampFilter checks if any timestamp is invalid, is older than 3 weeks, is more than 3 weeks in to the future
  * This filter rejects all assets if any timestamp fit a condition.
  */
 export class TimestampFilter extends AbstractFilter {
+  maxAge = new BigNumber(1000 * 60 * 60 * 24 * 7 * 3)
+  minAge = new BigNumber(1000 * 60 * 60 * 24 * 7 * 3).negated()
+
+  constructor (
+    protected readonly network: NetworkName,
+    protected readonly whale: WhaleApiClient,
+    protected readonly oracleId: string,
+    options?: TimestampFilterOptions
+  ) {
+    super(network, whale, oracleId)
+    this.maxAge = options?.maxAge ?? this.maxAge
+    this.minAge = options?.minAge ?? this.minAge
+  }
+
   async call (prices: AssetPrice[]): Promise<AssetPrice[]> {
     for (const price of prices) {
-      if (isInvalid(price)) {
+      if (isInvalid(price, this.maxAge, this.minAge)) {
         this.error(`TimestampFilter.isInvalid ${JSON.stringify(price)}`)
       }
     }
@@ -23,9 +44,7 @@ export class TimestampFilter extends AbstractFilter {
  *
  * @return boolean
  */
-function isInvalid (price: AssetPrice): boolean {
-  const maxAge = new BigNumber(1000 * 60 * 60 * 24 * 7 * 3) // 3 Weeks
-  const minAge = new BigNumber(1000 * 60 * 60 * 24 * 7 * 3).negated() // 3 Weeks
+function isInvalid (price: AssetPrice, maxAge: BigNumber, minAge: BigNumber): boolean {
   const currentTimestamp = new BigNumber(Date.now())
 
   if (price.timestamp.isNegative()) {
