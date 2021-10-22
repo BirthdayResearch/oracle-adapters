0ffcb6f99bd82f617da7a1ab3b12d70f6c0acf31
Joel-David Wong <4586209+joeldavidw@users.noreply.github.com>
2021-10-22 23:08:18 +0800

    migrated finnhubb (#77)

---
+++ adapters/__tests__/finnhubb.test.ts
@@ -0,0 +1,96 @@
+import nock from 'nock'
+import finnhubb from '../src/finnhubb'
+import BigNumber from 'bignumber.js'
+
+afterEach(() => {
+  jest.clearAllMocks()
+  nock.cleanAll()
+})
+
+it('should fetch price from finnhubb', async () => {
+  nock('https://finnhub.io/api/v1/quote')
+    .get('?symbol=AAPL&token=API_TOKEN')
+    .reply(200, function (_) {
+      return `{
+          "c": 261.74,
+          "h": 263.31,
+          "l": 260.68,
+          "o": 261.07,
+          "pc": 259.45,
+          "t": 1582641000 
+        }`
+    })
+
+  const prices = await finnhubb(['AAPL'], 'API_TOKEN')
+  expect(prices).toStrictEqual([
+    {
+      token: 'AAPL',
+      amount: new BigNumber(261.74),
+      currency: 'USD',
+      timestamp: new BigNumber(1582641000000)
+    }
+  ])
+})
+
+it('should fetch multiple prices from finnhubb', async () => {
+  nock('https://finnhub.io/api/v1/quote')
+    .get('?symbol=TSLA&token=API_TOKEN')
+    .reply(200, function (_) {
+      return `{
+          "c": 605.14,
+          "h": 263.31,
+          "l": 260.68,
+          "o": 261.07,
+          "pc": 259.45,
+          "t": 1582641000 
+        }`
+    })
+
+  nock('https://finnhub.io/api/v1/quote')
+    .get('?symbol=AAPL&token=API_TOKEN')
+    .reply(200, function (_) {
+      return `{
+          "c": 126,
+          "h": 263.31,
+          "l": 260.68,
+          "o": 261.07,
+          "pc": 259.45,
+          "t": 1582641000 
+        }`
+    })
+
+  nock('https://finnhub.io/api/v1/quote')
+    .get('?symbol=FB&token=API_TOKEN')
+    .reply(200, function (_) {
+      return `{
+          "c": 336.59,
+          "h": 263.31,
+          "l": 260.68,
+          "o": 261.07,
+          "pc": 259.45,
+          "t": 1582641000 
+        }`
+    })
+
+  const prices = await finnhubb(['TSLA', 'AAPL', 'FB'], 'API_TOKEN')
+  expect(prices).toStrictEqual([
+    {
+      token: 'TSLA',
+      amount: new BigNumber(605.14),
+      currency: 'USD',
+      timestamp: new BigNumber(1582641000000)
+    },
+    {
+      token: 'AAPL',
+      amount: new BigNumber(126),
+      currency: 'USD',
+      timestamp: new BigNumber(1582641000000)
+    },
+    {
+      token: 'FB',
+      amount: new BigNumber(336.59),
+      currency: 'USD',
+      timestamp: new BigNumber(1582641000000)
+    }
+  ])
+})
