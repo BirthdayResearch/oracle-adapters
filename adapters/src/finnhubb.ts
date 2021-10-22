fad46d274025610aad5adaa78f92e6450f400efe
Joel-David Wong <git@joeldavid.dev>
2021-10-22 19:01:14 +0800

    migrated finnhubb

---
+++ adapters/src/finnhubb.ts
@@ -0,0 +1,11 @@
+import { AssetPrice, fetchAsJson, newAssetPrice } from '@defichain/salmon-fetch'
+
+const URL = 'https://finnhub.io/api/v1/quote'
+
+export default async function (symbols: string[], apiToken: string): Promise<AssetPrice[]> {
+  return await Promise.all(symbols.map(async symbol => {
+    const res = await fetchAsJson(`${URL}?symbol=${symbol}&token=${apiToken}`)
+
+    return newAssetPrice(symbol, res.data.c, 'USD', res.data.t.multipliedBy(1000))
+  }))
+}
