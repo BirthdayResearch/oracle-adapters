7c6956d8924f3d92dff6e4382e2edc476e6ae894
Ahmed Hilali <monstrobishi@gmail.com>
2021-10-22 16:17:49 +0800

    Split tests

--- packages/salmon/__test__/publish.test.ts
+++ packages/salmon/__test__/publish.test.ts
@@ -13,215 +13,246 @@ let client: JsonRpcClient
 let whaleApiClient: WhaleApiClient
 let oracleId: string
 
-beforeEach(async () => {
-  await container.start()
-  await container.ain.waitForWalletCoinbaseMaturity()
+describe('salmon runner', () => {
+  beforeAll(async () => {
+    await container.start()
+    await container.ain.waitForWalletCoinbaseMaturity()
 
-  client = new JsonRpcClient(await container.ain.getCachedRpcUrl())
-  whaleApiClient = container.getWhaleApiClient()
+    client = new JsonRpcClient(await container.ain.getCachedRpcUrl())
+    whaleApiClient = container.getWhaleApiClient()
 
-  await client.wallet.sendToAddress(address.address, 1)
-  await container.ain.generate(1)
+    await client.wallet.sendToAddress(address.address, 1)
+    await container.ain.generate(1)
 
-  const symbols = ['BTC', 'ETH', 'DOGE'].map(x => ({ token: x, currency: 'USD' }))
-  oracleId = await client.oracle.appointOracle(address.address, symbols, { weightage: 1.0 })
-  await container.ain.generate(1)
+    const symbols = ['BTC', 'ETH', 'DOGE'].map(x => ({ token: x, currency: 'USD' }))
+    oracleId = await client.oracle.appointOracle(address.address, symbols, { weightage: 1.0 })
+    await container.ain.generate(1)
 
-  const height = await client.blockchain.getBlockCount()
-  await container.ain.generate(1)
+    const height = await client.blockchain.getBlockCount()
+    await container.ain.generate(1)
 
-  await waitForExpect(async () => {
-    const blocks = await whaleApiClient.blocks.list(1)
-    expect(blocks[0].height).toBeGreaterThanOrEqual(height)
-  }, 30000)
-})
+    await waitForExpect(async () => {
+      const blocks = await whaleApiClient.blocks.list(1)
+      expect(blocks[0].height).toBeGreaterThanOrEqual(height)
+    }, 30000)
+  })
 
-afterEach(async () => {
-  await container.stop()
-})
+  afterAll(async () => {
+    await container.stop()
+  })
 
-it('should publish prices', async () => {
-  const wallet = new SalmonWallet(address.privKey, 'regtest', whaleApiClient)
-  const salmon = new Salmon(oracleId, 'regtest', whaleApiClient, wallet)
+  it('should publish prices', async () => {
+    const wallet = new SalmonWallet(address.privKey, 'regtest', whaleApiClient)
+    const salmon = new Salmon(oracleId, 'regtest', whaleApiClient, wallet)
 
-  const prices: AssetPrice[] = [
-    {
-      token: 'BTC',
-      amount: new BigNumber(50000),
-      currency: 'USD',
-      timestamp: new BigNumber(Date.now())
-    },
-    {
-      token: 'ETH',
-      amount: new BigNumber(5000),
-      currency: 'USD',
-      timestamp: new BigNumber(Date.now())
-    },
-    {
-      token: 'DOGE',
-      amount: new BigNumber(0.5),
-      currency: 'USD',
-      timestamp: new BigNumber(Date.now())
-    }
-  ]
+    const prices: AssetPrice[] = [
+      {
+        token: 'BTC',
+        amount: new BigNumber(50000),
+        currency: 'USD',
+        timestamp: new BigNumber(Date.now())
+      },
+      {
+        token: 'ETH',
+        amount: new BigNumber(5000),
+        currency: 'USD',
+        timestamp: new BigNumber(Date.now())
+      },
+      {
+        token: 'DOGE',
+        amount: new BigNumber(0.5),
+        currency: 'USD',
+        timestamp: new BigNumber(Date.now())
+      }
+    ]
 
-  await salmon.publish(prices)
-  await container.ain.generate(1)
-
-  const oracleData = await client.oracle.getOracleData(oracleId)
-  expect(oracleData).toStrictEqual(
-    {
-      address: '0014e0dc4bafbea4d48f538ae526d8143a7159a510ee',
-      oracleid: oracleId,
-      priceFeeds: [
-        {
-          currency: 'USD',
-          token: 'BTC'
-        },
-        {
-          currency: 'USD',
-          token: 'DOGE'
-        },
-        {
-          currency: 'USD',
-          token: 'ETH'
-        }
-      ],
-      tokenPrices: [
-        {
-          amount: 50000,
-          currency: 'USD',
-          timestamp: expect.any(Number),
-          token: 'BTC'
-        },
-        {
-          amount: 0.5,
-          currency: 'USD',
-          timestamp: expect.any(Number),
-          token: 'DOGE'
-        },
-        {
-          amount: 5000,
-          currency: 'USD',
-          timestamp: expect.any(Number),
-          token: 'ETH'
-        }
-      ],
-      weightage: 1
-    }
-  )
-})
-
-it('should throw on NaN prices', async () => {
-  const wallet = new SalmonWallet(address.privKey, 'regtest', whaleApiClient)
-  const salmon = new Salmon(oracleId, 'regtest', whaleApiClient, wallet)
-
-  const prices: AssetPrice[] = [
-    {
-      token: 'BTC',
-      amount: new BigNumber(NaN),
-      currency: 'USD',
-      timestamp: new BigNumber(Date.now())
-    },
-    {
-      token: 'ETH',
-      amount: new BigNumber(5000),
-      currency: 'USD',
-      timestamp: new BigNumber(Date.now())
-    },
-    {
-      token: 'DOGE',
-      amount: new BigNumber(0.5),
-      currency: 'USD',
-      timestamp: new BigNumber(Date.now())
-    }
-  ]
-
-  await expect(async () => {
     await salmon.publish(prices)
-  }).rejects.toThrowError('AmountFilter.isInvalid ')
+    await container.ain.generate(1)
+
+    const oracleData = await client.oracle.getOracleData(oracleId)
+    expect(oracleData).toStrictEqual(
+      {
+        address: '0014e0dc4bafbea4d48f538ae526d8143a7159a510ee',
+        oracleid: oracleId,
+        priceFeeds: [
+          {
+            currency: 'USD',
+            token: 'BTC'
+          },
+          {
+            currency: 'USD',
+            token: 'DOGE'
+          },
+          {
+            currency: 'USD',
+            token: 'ETH'
+          }
+        ],
+        tokenPrices: [
+          {
+            amount: 50000,
+            currency: 'USD',
+            timestamp: expect.any(Number),
+            token: 'BTC'
+          },
+          {
+            amount: 0.5,
+            currency: 'USD',
+            timestamp: expect.any(Number),
+            token: 'DOGE'
+          },
+          {
+            amount: 5000,
+            currency: 'USD',
+            timestamp: expect.any(Number),
+            token: 'ETH'
+          }
+        ],
+        weightage: 1
+      }
+    )
+  })
+
+  it('should throw on NaN prices', async () => {
+    const wallet = new SalmonWallet(address.privKey, 'regtest', whaleApiClient)
+    const salmon = new Salmon(oracleId, 'regtest', whaleApiClient, wallet)
+
+    const prices: AssetPrice[] = [
+      {
+        token: 'BTC',
+        amount: new BigNumber(NaN),
+        currency: 'USD',
+        timestamp: new BigNumber(Date.now())
+      },
+      {
+        token: 'ETH',
+        amount: new BigNumber(5000),
+        currency: 'USD',
+        timestamp: new BigNumber(Date.now())
+      },
+      {
+        token: 'DOGE',
+        amount: new BigNumber(0.5),
+        currency: 'USD',
+        timestamp: new BigNumber(Date.now())
+      }
+    ]
+
+    await expect(async () => {
+      await salmon.publish(prices)
+    }).rejects.toThrowError('AmountFilter.isInvalid ')
+  })
+
+  it('should throw on negative prices', async () => {
+    const wallet = new SalmonWallet(address.privKey, 'regtest', whaleApiClient)
+    const salmon = new Salmon(oracleId, 'regtest', whaleApiClient, wallet)
+
+    const prices: AssetPrice[] = [
+      {
+        token: 'BTC',
+        amount: new BigNumber(50000),
+        currency: 'USD',
+        timestamp: new BigNumber(Date.now())
+      },
+      {
+        token: 'ETH',
+        amount: new BigNumber(-1),
+        currency: 'USD',
+        timestamp: new BigNumber(Date.now())
+      },
+      {
+        token: 'DOGE',
+        amount: new BigNumber(0.5),
+        currency: 'USD',
+        timestamp: new BigNumber(Date.now())
+      }
+    ]
+
+    await expect(async () => {
+      await salmon.publish(prices)
+    }).rejects.toThrowError('AmountFilter.isInvalid ')
+  })
 })
 
-it('should throw on negative prices', async () => {
-  const wallet = new SalmonWallet(address.privKey, 'regtest', whaleApiClient)
-  const salmon = new Salmon(oracleId, 'regtest', whaleApiClient, wallet)
+describe('salmon runner coingecko', () => {
+  beforeAll(async () => {
+    await container.start()
+    await container.ain.waitForWalletCoinbaseMaturity()
 
-  const prices: AssetPrice[] = [
-    {
-      token: 'BTC',
-      amount: new BigNumber(50000),
-      currency: 'USD',
-      timestamp: new BigNumber(Date.now())
-    },
-    {
-      token: 'ETH',
-      amount: new BigNumber(-1),
-      currency: 'USD',
-      timestamp: new BigNumber(Date.now())
-    },
-    {
-      token: 'DOGE',
-      amount: new BigNumber(0.5),
-      currency: 'USD',
-      timestamp: new BigNumber(Date.now())
-    }
-  ]
+    client = new JsonRpcClient(await container.ain.getCachedRpcUrl())
+    whaleApiClient = container.getWhaleApiClient()
 
-  await expect(async () => {
+    await client.wallet.sendToAddress(address.address, 1)
+    await container.ain.generate(1)
+
+    const symbols = ['BTC', 'ETH', 'DOGE'].map(x => ({ token: x, currency: 'USD' }))
+    oracleId = await client.oracle.appointOracle(address.address, symbols, { weightage: 1.0 })
+    await container.ain.generate(1)
+
+    const height = await client.blockchain.getBlockCount()
+    await container.ain.generate(1)
+
+    await waitForExpect(async () => {
+      const blocks = await whaleApiClient.blocks.list(1)
+      expect(blocks[0].height).toBeGreaterThanOrEqual(height)
+    }, 30000)
+  })
+
+  afterAll(async () => {
+    await container.stop()
+  })
+
+  it('should publish prices from coingecko', async () => {
+    const wallet = new SalmonWallet(address.privKey, 'regtest', whaleApiClient)
+    const salmon = new Salmon(oracleId, 'regtest', whaleApiClient, wallet)
+
+    const symbols = ['BTC', 'ETH', 'DOGE']
+    const prices = await coingecko(symbols)
+
     await salmon.publish(prices)
-  }).rejects.toThrowError('AmountFilter.isInvalid ')
-})
+    await container.ain.generate(1)
 
-it('should publish prices from coingecko', async () => {
-  const wallet = new SalmonWallet(address.privKey, 'regtest', whaleApiClient)
-  const salmon = new Salmon(oracleId, 'regtest', whaleApiClient, wallet)
-
-  const symbols = ['BTC', 'ETH', 'DOGE']
-  const prices = await coingecko(symbols)
-
-  await salmon.publish(prices)
-  await container.ain.generate(1)
-
-  const oracleData = await client.oracle.getOracleData(oracleId)
-  expect(oracleData).toStrictEqual(
-    {
-      address: '0014e0dc4bafbea4d48f538ae526d8143a7159a510ee',
-      oracleid: oracleId,
-      priceFeeds: [
-        {
-          currency: 'USD',
-          token: 'BTC'
-        },
-        {
-          currency: 'USD',
-          token: 'DOGE'
-        },
-        {
-          currency: 'USD',
-          token: 'ETH'
-        }
-      ],
-      tokenPrices: [
-        {
-          amount: prices[0].amount.toNumber(),
-          currency: 'USD',
-          timestamp: expect.any(Number),
-          token: 'BTC'
-        },
-        {
-          amount: prices[2].amount.toNumber(),
-          currency: 'USD',
-          timestamp: expect.any(Number),
-          token: 'DOGE'
-        },
-        {
-          amount: prices[1].amount.toNumber(),
-          currency: 'USD',
-          timestamp: expect.any(Number),
-          token: 'ETH'
-        }
-      ],
-      weightage: 1
-    }
-  )
+    const oracleData = await client.oracle.getOracleData(oracleId)
+    expect(oracleData).toStrictEqual(
+      {
+        address: '0014e0dc4bafbea4d48f538ae526d8143a7159a510ee',
+        oracleid: oracleId,
+        priceFeeds: [
+          {
+            currency: 'USD',
+            token: 'BTC'
+          },
+          {
+            currency: 'USD',
+            token: 'DOGE'
+          },
+          {
+            currency: 'USD',
+            token: 'ETH'
+          }
+        ],
+        tokenPrices: [
+          {
+            amount: prices[0].amount.toNumber(),
+            currency: 'USD',
+            timestamp: expect.any(Number),
+            token: 'BTC'
+          },
+          {
+            amount: prices[2].amount.toNumber(),
+            currency: 'USD',
+            timestamp: expect.any(Number),
+            token: 'DOGE'
+          },
+          {
+            amount: prices[1].amount.toNumber(),
+            currency: 'USD',
+            timestamp: expect.any(Number),
+            token: 'ETH'
+          }
+        ],
+        weightage: 1
+      }
+    )
+  })
 })
