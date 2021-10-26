d6ad44a0525656011bb421c0957feb05e518e129
websterlcl <w.canonbrother@gmail.com>
2021-10-26 15:22:02 +0800

    replace fetch by fetchasjson, remove nodefetch from package.json

--- adapters/src/coinmarketcap.ts
+++ adapters/src/coinmarketcap.ts
@@ -1,20 +1,13 @@
-import { AssetPrice, newAssetPrice } from '@defichain/salmon-fetch'
+import { AssetPrice, newAssetPrice, fetchAsJson, FetchResponse } from '@defichain/salmon-fetch'
 import BigNumber from 'bignumber.js'
-import fetch from 'node-fetch'
-import {
-  JellyfishJSON
-} from '@defichain/jellyfish-json'
-
 const COINMARKETCAP_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 
 export default async function (symbols: string[], apiToken = ''): Promise<AssetPrice[]> {
   const fetchPath = `${COINMARKETCAP_URL}?symbol=${symbols.join(',')}&CMC_PRO_API_KEY=${apiToken}`
-  const response = await fetch(fetchPath, {
+  const res: FetchResponse = await fetchAsJson(fetchPath, {
     method: 'GET'
   })
-
-  const text = await response.text()
-  const json = JellyfishJSON.parse(text, 'bignumber').data
+  const json = res.data.data
   return Object.keys(json).map((asset: any) => {
     const data = json[asset]
     const timestamp = new BigNumber(Date.parse(data.last_updated))
