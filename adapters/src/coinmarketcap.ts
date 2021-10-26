bef0ae276e5c7a3285b79fab9b5c2271db64f2e4
websterlcl <w.canonbrother@gmail.com>
2021-10-26 17:12:05 +0800

    set apitoken mandatory as no more process.env

--- adapters/src/coinmarketcap.ts
+++ adapters/src/coinmarketcap.ts
@@ -2,7 +2,7 @@ import { AssetPrice, newAssetPrice, fetchAsJson, FetchResponse } from '@defichai
 import BigNumber from 'bignumber.js'
 const COINMARKETCAP_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 
-export default async function (symbols: string[], apiToken = ''): Promise<AssetPrice[]> {
+export default async function (symbols: string[], apiToken: string): Promise<AssetPrice[]> {
   const fetchPath = `${COINMARKETCAP_URL}?symbol=${symbols.join(',')}&CMC_PRO_API_KEY=${apiToken}`
   const res: FetchResponse = await fetchAsJson(fetchPath, {
     method: 'GET'
