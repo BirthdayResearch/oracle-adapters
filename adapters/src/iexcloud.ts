7d79e9dd08e522bdcb495e421c4f956318e40cff
Eric <tzeweiwee@gmail.com>
2022-06-06 18:15:56 +0800

    chore(iexcloud): update iexcloud adapter to use quote endpoint (#264)
    * update iexcloud adapter to use quote endpoint
    
    * use fetchresponse interface
    
    * added validations to check symbols length and matching requested symbols

--- adapters/src/iexcloud.ts
+++ adapters/src/iexcloud.ts
@@ -1,14 +1,31 @@
-import { AssetPrice, fetchAsJson, newAssetPrice } from '@defichain/salmon-fetch'
+import { AssetPrice, fetchAsJson, newAssetPrice, FetchResponse } from '@defichain/salmon-fetch'
 
-const URL = 'https://cloud.iexapis.com/stable/tops/last'
+const URL = 'https://cloud.iexapis.com/stable/stock'
+
+function fetchPath (symbol: string, apiToken: string): string {
+  return `${URL}/${symbol}/quote?token=${apiToken}`
+}
 
 export default async function (symbols: string[], apiToken: string): Promise<AssetPrice[]> {
-  const fetchPath = `${URL}?symbols=${symbols.join(',')}&token=${apiToken}`
-  const response = await fetchAsJson(fetchPath, {
-    method: 'GET'
-  })
+  const responses = await Promise.all(symbols.map(
+    async (symbol: string) => await fetchAsJson(fetchPath(symbol, apiToken), { method: 'GET' })
+  ))
 
-  return response.data.map((x: any): AssetPrice => {
-    return newAssetPrice(x.symbol, x.price, 'USD', x.time)
-  })
+  const stockPrices = responses
+    .flatMap((res: FetchResponse) => res.data)
+    .map((x: any): AssetPrice => {
+      const { symbol, latestPrice, latestUpdate } = x
+      return newAssetPrice(symbol, latestPrice, 'USD', latestUpdate)
+    })
+
+  if (stockPrices.length !== symbols.length) {
+    throw new Error('iexcloud.invalidNumberOfSymbols')
+  }
+
+  const matchingRequestedSymbols = stockPrices.filter((stock) => !symbols.includes(stock.token)).length === 0
+  if (!matchingRequestedSymbols) {
+    throw new Error('iexcloud.invalidSymbols')
+  }
+
+  return stockPrices
 }
